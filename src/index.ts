7c345c7c55fd944fc26cce354e85aa94e278a0f0
devTam <tammy.batubo@gmail.com>
2021-06-14 10:29:50 +0100

    setup tsconfig file and web socket using socket.io

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
-const express = require('express');
-const path = require('path');
+import express from 'express'; 
+import path from 'path';
+import http from 'http';
+const socketio = require('socket.io');
 
-const app = express();
+const app = express(); 
+const server = http.createServer(app);
+const io = socketio(server);
 
 const port = process.env.PORT || 5000;
 app.use(express.json());
@@ -10,6 +14,10 @@ const publicDir = path.join(__dirname, '../public');
 
 app.use(express.static(publicDir));
 
-app.listen(port, () => {
+io.on('connection', () => {
+    console.log('new web socket connection')
+})
+
+server .listen(port, () => {
     console.log(`server listening on port: ${port}`);
 });
